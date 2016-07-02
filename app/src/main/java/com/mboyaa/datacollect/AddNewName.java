4c79579cbd9ad87073a4afa57b5db9127ea011dc
mboya <atupelemboya@gmail.com>
2016-07-02 13:45:15 +0200

    Adding new page

--- app/src/main/java/com/mboyaa/datacollect/AddNewName.java
+++ app/src/main/java/com/mboyaa/datacollect/AddNewName.java
@@ -6,6 +6,8 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class AddNewName extends AppCompatActivity {
 
@@ -16,12 +18,24 @@ public class AddNewName extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        Button saveBtn;
+
+        saveBtn = (Button)findViewById(R.id.saveNameBtn);
+
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
+            }
+        });
+
+        saveBtn.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(),"Saved",Toast.LENGTH_LONG).show();
             }
         });
     }
